0da2cb8ecd09309ef009be5c6631e4619a60e7ee
pkallinos <31747117+pkallinos@users.noreply.github.com>
2017-09-29 15:02:14 -0400

    Code Monkey: Includes
    fixed existing includes

--- inc/navigation.php
+++ inc/navigation.php
@@ -2,10 +2,11 @@
 <?php 
 echo
  "
-<ul>
-<li> <a href="index.php">Home</a> </li>
-<li> <a href="jerrydai.php">Jerry Dai</a> </li>
-<li> <a href="philipkallinos.php">Philip Kallinos</a> </li>
-<li> <a href="daniellabloom.php">Daniella Bloom </a> </li> </ul> 
+		<ul>
+		<li> <a href="index.php">Home</a> </li>
+		<li> <a href="jerrydai.php">Jerry Dai</a> </li>
+		<li> <a href="philipkallinos.php">Philip Kallinos</a> </li>
+		<li> <a href="daniellabloom.php">Daniella Bloom </a> </li> 
+		</ul> 
 "
 ?>
