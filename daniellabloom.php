43af7f877ac6e2cd4893f79e8770488d04580c1c
dbloom4 <31747138+dbloom4@users.noreply.github.com>
2017-09-29 16:28:32 -0400

    Update daniellabloom.php

--- daniellabloom.php
+++ daniellabloom.php
@@ -23,18 +23,23 @@
               <?php include 'inc/navigation.php'; ?>
          </nav>
 
-	<h3>Early Life</h3>
+	<h3>Personal Bio</h3>
 
-		<p>I was born and grew up in New York City on the Upper West Side. I lived with my mom, dad, brother Jamie and brother Paul. I started doing gymnastics recreationally and when I was 8 I joined a team. I stopped doing gymnastics right before highschool started. </p>
-	
+		<p>I was born and grew up in New York City on the Upper West Side. I lived withmy mom, dad, brother Jamie and brother
+		Paul. I started doing gymnastics recreationally and when I was 8 I joined a team. I stopped doing gymnastics right 
+		before highschool started. </p>
+	<h3>Work Experience</h3>
+		<p> I started volunteering at Memorial Sloan Kettering Cancer Center in 9th grade and continued to do so through high school.
+		Summer 2014 and 2015 I worked as a Pathology assistant, summer 2016 I worked there as a Session Assistant and then over this past summer I had
+		an internship with a start up company and assisted a professor with research at the U of R. The internship focused on market research in order to gain insight for the company and the research focused on brand valuation.</p> 
+
 	<h3>Education</h3>
-		<p>I went to elementary school at P.S.6 which was located on the upper east side. Because I went to school on the east side I then had access to the other schools in the district so for middle school I went to East Side Middle. For highschool I went to Eleanor Roosevelt High School also located on the upper east side.</p>
+		<p>I went to elementary school at P.S.6 which was located on the upper east side. Because I went to school on the east side I then had access to the other schools in the district so for
+		middle school I went to East Side Middle. For highschool I went to Eleanor Roosevelt High School also located on the upper east side.
+		I now attend the University of Rochester and I am majoring in business analytics and minoring in computer science with a focus on web design.</p>
 	
-	<h3>Internships and Jobs</h3>
-		<p> I started volunteering at Memorial Sloan Kettering Cancer Center in 9th grade and continued to do so through high school. Summer 2014 and 2015 I worked as a Pathology assistant, summer 2016 I worked there as a Session Assistant and then over this past summer I had an internship with a start up company and assisted a professor with research at the U of R. The internship focused on market research in order to gain insight for the company and the research focused on brand valuation.</p> 
-
-	<h3>Major and Minor</h3>
-		<p> I am majoring in business analytics and minoring in computer science with a focus on web design.</p>
+	
+	
 
 </body>
 
