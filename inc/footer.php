973cb3718ddca90639ad4229b180bddb428beece
pkallinos <31747117+pkallinos@users.noreply.github.com>
2017-10-01 19:51:50 -0400

    Code Monkey: include footer

--- inc/footer.php
+++ inc/footer.php
@@ -1,7 +1,7 @@
 <?php 
 echo
 '	
-		 <div class="row align-items-center">
+		 <div class="column row align-items-center">
          	<div class="col-md-12 col-sm-12 col-xs-12">
 				<h2>CSC 174 - Assignment 5</h2>
 				<h2>Created by Jerry Dai, Philip Kallinos and Daniella Bloom</h2>
