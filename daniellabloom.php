80a40b0866c0b03744a725ad10ac787ae822109b
pkallinos <31747117+pkallinos@users.noreply.github.com>
2017-10-01 18:10:11 -0400

    Code Monkey: Fix error on codes

--- daniellabloom.php
+++ daniellabloom.php
@@ -7,37 +7,50 @@
 		<?php include 'inc/head.php'; ?>
     </head>
 
-<body>
+
+    <body>
+
+        <div class="container">
+
+            <header>
+                <div class="heading">
+                    <h1>Daniella Bloom</h1>
+                </div><!--closes heading class-->
+            </header>
+
+            <nav class="menu">
+                <?php include 'inc/navigation.php'; ?>
+            </nav>
+
+            <div class="content">
+                <h2><figure><img src="images/daniella.jpg" alt="Daniella Bloom"><figcaption>Daniella</figcaption></figure></h2>
+
+                <h2>Background</h2>
+                <p>
+                    I was born and grew up in New York City on the Upper West Side. I lived withmy mom, dad, brother Jamie and brother Paul. I started doing gymnastics recreationally and when I was 8 I joined a team. I stopped doing gymnastics right before highschool started.
+                </p>
+
+                <h2>Work Experience</h2>
+                <p>
+                    I started volunteering at Memorial Sloan Kettering Cancer Center in 9th grade and continued to do so through high school.
+                    Summer 2014 and 2015 I worked as a Pathology assistant, summer 2016 I worked there as a Session Assistant and then over this past summer I had an internship with a start up company and assisted a professor with research at the U of R. The internship focused on market research in order to gain insight for the company and the research focused on brand valuation.
+                </p>
 
 
-<header>
-<h1>Daniella Bloom</h1>
+                <h2>Education</h2>
+                <p>
+                    I went to elementary school at P.S.6 which was located on the upper east side. Because I went to school on the east side I then had access to the other schools in the district so formiddle school I went to East Side Middle. For highschool I went to Eleanor Roosevelt High School also located on the upper east side. I now attend the University of Rochester and I am majoring in business analytics and minoring in computer science with a focus on web design.
+                </p>
 
-</header>
+  
+            </div><!--closes content class-->
 
+            <footer>
+                <?php include 'inc/footer.php'; ?>
+            </footer>
 
-	<nav class="menu">
-              <?php include 'inc/navigation.php'; ?>
-         </nav>
-<h2><figure><img src="images/daniella.jpg" alt="Daniella Bloom"><figcaption>Daniella</figcaption></figure></h2>
-	<h3>Personal Bio</h3>
+        </div><!-- closes the container class-->
 
-		<p>I was born and grew up in New York City on the Upper West Side. I lived withmy mom, dad, brother Jamie and brother
-		Paul. I started doing gymnastics recreationally and when I was 8 I joined a team. I stopped doing gymnastics right 
-		before highschool started. </p>
-	<h3>Work Experience</h3>
-		<p> I started volunteering at Memorial Sloan Kettering Cancer Center in 9th grade and continued to do so through high school.
-		Summer 2014 and 2015 I worked as a Pathology assistant, summer 2016 I worked there as a Session Assistant and then over this past summer I had
-		an internship with a start up company and assisted a professor with research at the U of R. The internship focused on market research in order to gain insight for the company and the research focused on brand valuation.</p> 
-
-	<h3>Education</h3>
-		<p>I went to elementary school at P.S.6 which was located on the upper east side. Because I went to school on the east side I then had access to the other schools in the district so for
-		middle school I went to East Side Middle. For highschool I went to Eleanor Roosevelt High School also located on the upper east side.
-		I now attend the University of Rochester and I am majoring in business analytics and minoring in computer science with a focus on web design.</p>
-	
-	
-	
-
-</body>
+    </body>
 
 </html>
