bd5559cefc4bbbd2dbedc95db479da239edb7dab
pkallinos <31747117+pkallinos@users.noreply.github.com>
2017-10-01 18:38:04 -0400

    Code monkey: incldues- nav and footer

--- inc/footer.php
+++ inc/footer.php
@@ -1,7 +1,11 @@
 <?php 
 echo
-"
-		<h2>CSC 174 - Assignment 5</h2>
-		<h2>Created by Jerry Dai, Philip Kallinos and Daniella Bloom</h2>
-"
+'	
+		 <div class="row align-items-center">
+         	<div class="col-md-12 col-sm-12 col-xs-12">
+				<h2>CSC 174 - Assignment 5</h2>
+				<h2>Created by Jerry Dai, Philip Kallinos and Daniella Bloom</h2>
+			</div>
+		</div>
+'
 ?>
--- inc/navigation.php
+++ inc/navigation.php
@@ -1,12 +1,21 @@
 
 <?php 
 echo
- '
-		<ul>
-		<li> <a href="index.php">Home</a> </li>
-		<li> <a href="jerrydai.php">Jerry Dai</a> </li>
-		<li> <a href="philipkallinos.php">Philip Kallinos</a> </li>
-		<li> <a href="daniellabloom.php">Daniella Bloom </a> </li> 
-		</ul> 
+ '	
+ 	            <div class="nav-class">
+                <nav class="navbar navbar-inverse navbar-static-top" role="navigation">
+                    <div class="navbar-collapse collapse">
+                        <div class="nav-wrapper">
+                            <ul class="nav navbar-nav pull-right">
+                                <li> <a href="index.php">Home</a> </li>
+								<li> <a href="jerrydai.php">Jerry Dai</a> </li>
+								<li> <a href="philipkallinos.php">Philip Kallinos</a> </li>
+								<li> <a href="daniellabloom.php">Daniella Bloom </a> </li> 
+                            </ul>
+                        </div>
+                    </div><!-- close navbar-collapse collapse-->
+
+                </nav>
+            </div><!-- close nav-class -->
 '
 ?>
