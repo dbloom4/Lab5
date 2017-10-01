0d862431c647e40e4b07361382967c612e72dc66
pkallinos <31747117+pkallinos@users.noreply.github.com>
2017-10-01 18:04:58 -0400

    Code Monkey: Includes

--- inc/footer.php
+++ inc/footer.php
@@ -2,6 +2,6 @@
 echo
 "
 		<h2>CSC 174 - Assignment 5</h2>
-		<h2>Created by Jerry Dai, Phillip Kallinos and Daniella Bloom</h2>
+		<h2>Created by Jerry Dai, Philip Kallinos and Daniella Bloom</h2>
 "
 ?>
--- inc/head.php
+++ inc/head.php
@@ -3,14 +3,14 @@ echo
 '
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-    	<link href="css/styles.css" rel="stylesheet">
-        <link href="css/navigation.css" rel="stylesheet">
         <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
         <script type="text/javascript" src="static/jquery.js"></script>
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <!-- Latest compiled and minified JavaScript -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+		<link href="css/styles.css" rel="stylesheet">
+        <link href="css/navigation.css" rel="stylesheet">
 '
 
 ?>
