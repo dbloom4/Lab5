43c3fc7a371a0ce4fb85572e4674359bc64634e9
Jerry Dai <jdai315@gmail.com>
2017-10-01 20:21:37 -0400

    Designer: added Bootstrap HTML classes, added custom styles and navigation stylesheets

--- index.php
+++ index.php
@@ -24,17 +24,20 @@
             <div class="content">
 
                 <div class="row align-items-center">
-                    <div class="col-md-12 col-sm-12 col-xs-12">
+                    <div class="column col-md-12 col-sm-12 col-xs-12">
+                        <!--source: http://www.cnn.com/2016/05/04/asia/gallery/inside-north-korea/index.html -->
                         <figure><img src="images/pyongyang.jpg" alt="Pyongyang"></figure>
                     </div>
                 </div>
 
+                <hr>
+                
                 <div class="row align-items-center">
-                    <div class="col-md-4 col-sm-4 col-xs-12">
-                        <figure><img src="images/jerry.png" alt="Jerry Dai"></figure>
+                    <div class="column col-md-4 col-sm-4 col-xs-12">
+                        <figure><img class="headshot" src="images/jerry.png" alt="Jerry Dai"></figure>
                     </div>
-                    <div class="col-md-8 col-sm-8 col-xs-12">
-                        <div class="well">
+                    <div class="column col-md-8 col-sm-8 col-xs-12">
+                        <div class="blurb">
                             <h2><a href="jerrydai.php">Jerry Dai</a></h2>
                             <p>
                                 Hi my name is Jerry and I am a junior at the U of R. I'm from Queens, NY but my parents moved to Brooklyn from China when they were in their early 20s. My hobbies are music, lifting, and cooking. My favorite foods include halal, vodka slices, and sushi. I am double majoring in Business and Computer Science and I'm interested in working in finance in the future.
@@ -43,13 +46,15 @@
                         </div><!--closes well class-->
                     </div>
                 </div>
+                
+                <hr>
 
                 <div class="row align-items-center">
-                    <div class="col-md-4 col-sm-4 col-xs-12">
-                        <figure><img src="images/philip.jpg" alt="Philip Kallinos"></figure>
+                    <div class="column col-md-4 col-sm-4 col-xs-12">
+                        <figure><img class="headshot" src="images/philip.jpg" alt="Philip Kallinos"></figure>
                     </div>
-                    <div class="col-md-8 col-sm-8 col-xs-12">
-                        <div class="well">
+                    <div class="column col-md-8 col-sm-8 col-xs-12">
+                        <div class="blurb">
                             <h2><a href="philipkallinos.php">Philip Kallinos</a></h2>
                             <p>
                                 I am from Lindenhurst, New York. My parents are both immigrants. My father is from Cyprus and my mother is from Thailand. I also have one older sister. I am currently a junior at the University of Rochester. I am studying Business with a concentration in Finance. I plan on completing a minor in math and computer science, hopefully.
@@ -58,13 +63,15 @@
                         </div><!--closes well class-->
                     </div>
                 </div>
+                
+                <hr>
 
                 <div class="row align-items-center">
-                    <div class="col-md-4 col-sm-4 col-xs-12">
-                        <figure><img src="images/daniella.jpg" alt="Daniella Bloom"></figure>
+                    <div class="column col-md-4 col-sm-4 col-xs-12">
+                        <figure><img class="headshot" src="images/daniella.jpg" alt="Daniella Bloom"></figure>
                     </div>
-                    <div class="col-md-8 col-sm-8 col-xs-12">
-                        <div class="well">
+                    <div class="column col-md-8 col-sm-8 col-xs-12">
+                        <div class="blurb">
                             <h2><a href="daniellabloom.php">Daniella Bloom</a></h2>
                             <p>
                                 Daniella was born and grew up in New York City on the Upper West Side. She lived with her mom, dad, brother Jamie and brother Paul. She started doing gymnastics recreationally and when she was 8, she joined a team. She stopped doing gymnastics right before highschool started.
@@ -73,6 +80,8 @@
                         </div><!--closes well class-->
                     </div>
                 </div>
+                
+                <hr>
 
             </div><!--closes content class-->
 
