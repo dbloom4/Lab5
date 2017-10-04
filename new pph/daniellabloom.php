28c667016aae2eee6940a2b8312deff05980b3af
pkallinos <31747117+pkallinos@users.noreply.github.com>
2017-10-04 17:22:38 -0400

    code monkey: tried to do .inc

---
+++ new pph/daniellabloom.php
@@ -0,0 +1,64 @@
+<?php
+    $currentTitle = "Daniella Bloom";
+    include "inc/top-part.inc";
+?>
+
+<?php include 'inc/navigation.inc'; ?>
+
+<div class="content">
+
+<div class="row align-items-center">
+    <div class="column col-md-4 col-sm-4 col-xs-12">
+        <figure><img class="headshot" src="images/daniella.jpg" alt="Daniella Bloom"></figure>
+    </div>
+    <div class="column col-md-8 col-sm-8 col-xs-12">
+        <div class="blurb">
+            <h2>Personal Bio</h2>
+            <p>
+                I was born and grew up in New York City on the Upper West Side. I lived withmy mom, dad, brother Jamie and brother Paul. I started doing gymnastics recreationally and when I was 8 I joined a team. I stopped doing gymnastics right before highschool started.
+            </p>
+        </div><!--closes well class-->
+    </div>
+</div>
+
+<hr>
+
+<div class="row align-items-center">
+    <div class="column col-md-12 col-sm-12 col-xs-12">
+        <div class="blurb">
+            <h2>Work Experience</h2>
+            <p>
+                I started volunteering at Memorial Sloan Kettering Cancer Center in 9th grade and continued to do so through high school.
+                Summer 2014 and 2015 I worked as a Pathology assistant, summer 2016 I worked there as a Session Assistant and then over this past summer I had an internship with a start up company and assisted a professor with research at the U of R. The internship focused on market research in order to gain insight for the company and the research focused on brand valuation.
+            </p>
+        </div><!--closes well class-->
+    </div>
+</div>
+
+<hr>
+
+<div class="row align-items-center">
+    <div class="column col-md-12 col-sm-12 col-xs-12">
+        <div class="blurb">
+            <h2>Education</h2>
+            <p>
+                I went to elementary school at P.S.6 which was located on the upper east side. Because I went to school on the east side I then had access to the other schools in the district so formiddle school I went to East Side Middle. For highschool I went to Eleanor Roosevelt High School also located on the upper east side. I now attend the University of Rochester and I am majoring in business analytics and minoring in computer science with a focus on web design.
+            </p>
+        </div><!--closes well class-->
+    </div>
+</div>
+
+<hr>
+
+</div><!--closes content class-->
+
+<footer>
+<?php include 'inc/footer.php'; ?>
+</footer>
+
+</div><!--closes the container class-->
+
+<?php iniclude "inc/scripts.inc"; ?>
+</body>
+
+</html>
