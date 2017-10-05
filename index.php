f5e19eb8481fb7466598ac2e139384af37cd3a64
pkallinos <31747117+pkallinos@users.noreply.github.com>
2017-10-04 20:06:01 -0400

    Code Monkey: change links

--- index.php
+++ index.php
@@ -22,11 +22,11 @@
     </div>
     <div class="column col-md-8 col-sm-8 col-xs-12">
         <div class="blurb">
-            <h2><a href="jerrydai.inc">Jerry Dai</a></h2>
+            <h2><a href="jerrydai.php">Jerry Dai</a></h2>
             <p>
                 Hi my name is Jerry and I am a junior at the U of R. I'm from Queens, NY but my parents moved to Brooklyn from China when they were in their early 20s. My hobbies are music, lifting, and cooking. My favorite foods include halal, vodka slices, and sushi. I am double majoring in Business and Computer Science and I'm interested in working in finance in the future.
             </p>
-            <a href="jerrydai.inc"> Read more about Jerry here</a>
+            <a href="jerrydai.php"> Read more about Jerry here</a>
         </div><!--closes well class-->
     </div>
 </div>
@@ -39,11 +39,11 @@
     </div>
     <div class="column col-md-8 col-sm-8 col-xs-12">
         <div class="blurb">
-            <h2><a href="philipkallinos.inc">Philip Kallinos</a></h2>
+            <h2><a href="philipkallinos.php">Philip Kallinos</a></h2>
             <p>
                 I am from Lindenhurst, New York. My parents are both immigrants. My father is from Cyprus and my mother is from Thailand. I also have one older sister. I am currently a junior at the University of Rochester. I am studying Business with a concentration in Finance. I plan on completing a minor in math and computer science, hopefully.
             </p>
-            <a href="philipkallinos.inc">Read more about Philip here </a>
+            <a href="philipkallinos.php">Read more about Philip here </a>
         </div><!--closes well class-->
     </div>
 </div>
@@ -56,11 +56,11 @@
     </div>
     <div class="column col-md-8 col-sm-8 col-xs-12">
         <div class="blurb">
-            <h2><a href="daniellabloom.inc">Daniella Bloom</a></h2>
+            <h2><a href="daniellabloom.php">Daniella Bloom</a></h2>
             <p>
                 Daniella was born and grew up in New York City on the Upper West Side. She lived with her mom, dad, brother Jamie and brother Paul. She started doing gymnastics recreationally and when she was 8, she joined a team. She stopped doing gymnastics right before highschool started.
             </p>
-            <a href="daniellabloom.inc"> Read more about Daniella here </a>
+            <a href="daniellabloom.php"> Read more about Daniella here </a>
         </div><!--closes well class-->
     </div>
 </div>
